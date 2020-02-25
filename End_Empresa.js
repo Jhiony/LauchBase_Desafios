2a445c997369dde7df38a68d67a3715c410b981b
Jhiony <jhiony.stolarski@gmail.com>
2020-02-25 15:54:29 -0300

    Incluido o arquivo para informar o endereço da empresa.

---
+++ End_Empresa.js
@@ -0,0 +1,15 @@
+// Curso LauchBase - Desafio 1-2 (Objetos e vetores)
+
+const empresa = 
+    {        
+        Nome: "Rocketseat",
+        Cor: "Roxo",
+        Foco: "Programação",
+        Endereço: 
+        {
+            Rua: "Rua Guilherme Gembala",
+            Número: "260"
+        }
+    }
+
+    console.log (`A empresa ${empresa.Nome} está localizada em ${empresa.Endereço.Rua}, ${empresa.Endereço.Número}`)
