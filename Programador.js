7c2b88945164442ee62975dda8f32589625c714b
Jhiony <jhiony.stolarski@gmail.com>
2020-02-25 16:25:09 -0300

    Incluido arquivo informar sobre o programador.

---
+++ Programador.js
@@ -0,0 +1,25 @@
+
+
+const programador =
+    {        
+        nome: "Jhiony",
+        idade: "35"
+    }
+
+const tecnologias = 
+    [
+        {
+            nome: "JavaScript",
+            especialidade: "Web/Mobile"        
+        },
+        {
+            nome: "C++",
+            especialidade: "Desktop"        
+        },
+        {
+            nome: "Python",
+            especialidade: "Data Science"        
+        }
+    ]
+
+    console.log(`O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${tecnologias[0].nome} com especialidade em ${tecnologias[0].especialidade}.`)
